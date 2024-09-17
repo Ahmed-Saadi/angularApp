28ff04387950c8e04d8d7b97dfe0bfa241a6f6b2
ahmed saadi <ahmadsaadi7130@gmail.com>
2024-09-17 16:00:47 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CostumersComponent } from './costumers/costumers.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'costumers', component: CostumersComponent },
+  { path: 'products', component: ProductsComponent },
+];
