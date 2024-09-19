7fc7eb690def523a1313e559d480cb37d1e2a0d4
ahmed saadi <ahmadsaadi7130@gmail.com>
2024-09-19 23:37:39 +0100

    second commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../services/product.service';
+import { Observer } from 'rxjs';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  products!: Product[];
+  errorMessage!: String;
 
+  constructor(private service: ProductService) {}
+
+  ngOnInit(): void {
+    this.handleGetAllProducts();
+    console.log('this happend ');
+  }
+
+  handleGetAllProducts() {
+    this.service.getAllProducts().subscribe({
+      next: (data: Product[]) => {
+        this.products = data;
+      },
+      error: (err) => {
+        this.errorMessage = err;
+      },
+    });
+  }
+
+  handleDeleteProduct(p: Product) {
+    let conf = confirm("are u sure maaann")
+    if(!conf) return ;
+    this.service.deleteProduct(p.id).subscribe({
+      next: (data: Product[]) => {
+        this.products = data;
+      },
+    });
+  }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { Product } from '../model/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private products!: Product[];
+
+  constructor() {
+    this.products = [
+      { id: 1, name: 'pc', price: 3500 },
+      { id: 2, name: 'phone', price: 1500 },
+      { id: 3, name: 'camera', price: 1000 },
+    ];
+  }
+
+
+
+  public getAllProducts () : Observable <Product[]>{
+          return of(this.products);
+  }
+
+  public deleteProduct(index:number ) : Observable<Product[]>{
+    this.products = this.products.filter(p => p.id != index ) 
+    return of(this.products);
+  }
+}
+
