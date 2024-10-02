794e29f4d03f83a27915666d18a71e39992a3bdc
ahmed saadi <ahmadsaadi7130@gmail.com>
2024-10-03 00:00:58 +0100

    finished the product page no error  in this version

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,25 +2,41 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../services/product.service';
 import { Observer } from 'rxjs';
-import { Product } from '../model/product.model';
+import { PageProduct, Product } from '../model/product.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-
   products!: Product[];
+  currentPage: number = 0;
+  pageSize: number = 5;
+  totalPage: number = 0;
   errorMessage!: String;
+  searchFormGroup!: FormGroup;
+  isSearch : boolean =false ;
 
-  constructor(private service: ProductService) {}
+  constructor(private service: ProductService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.handleGetAllProducts();
-    console.log('this happend ');
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control(null),
+    });
+    this.handleGetPageProducts();
+  }
+
+  handleGetPageProducts() {
+    this.service.getPageProducts(this.currentPage, this.pageSize).subscribe({
+      next: (data: PageProduct) => {
+        this.products = data.products;
+        this.totalPage = data.totalPages;
+      },
+    });
   }
 
   handleGetAllProducts() {
@@ -35,16 +51,44 @@ export class ProductsComponent {
   }
 
   handleDeleteProduct(p: Product) {
-    let conf = confirm("are u sure maaann")
-    if(!conf) return ;
+    let conf = confirm('are u sure maaann');
+    if (!conf) return;
     this.service.deleteProduct(p.id).subscribe({
-      next: (data: Product[]) => {
-        this.products = data;
+      next: (data: boolean) => {
+        let index = this.products.indexOf(p);
+        this.products.splice(index, 1);
       },
     });
   }
-  handlePromotion(p:Product) :void{
-      let index = this.products.indexOf(p);
-      this.products[index].promotion = !this.products[index].promotion
+  handlePromotion(p: Product) {
+    const promotion: boolean = p.promotion;
+    this.service.setPromotion(p).subscribe({
+      next: (data: Product) => {
+        p.promotion = !promotion;
+      },
+    });
+  }
+
+  handleSubmitForm() {
+    this.isSearch = true;
+    let keyword = this.searchFormGroup.value.keyword;
+    this.service
+      .searchProduct(keyword, this.currentPage, this.pageSize)
+      .subscribe({
+        next: (data) => {
+          this.products = data.products;
+          this.totalPage = data.totalPages;
+        },
+      });
+  }
+
+  public gotoPage(index: number) {
+    this.currentPage = index;
+    if(this.isSearch){
+      this.handleSubmitForm();
+    }else{
+      this.handleGetPageProducts();
     }
+    
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
-import { Product } from '../model/product.model';
+import { PageProduct, Product } from '../model/product.model';
+import { UUID } from 'angular2-uuid';
 
 @Injectable({
   providedIn: 'root',
@@ -10,21 +11,73 @@ export class ProductService {
 
   constructor() {
     this.products = [
-      { id: 1, name: 'pc', price: 3500,promotion: false},
-      { id: 2, name: 'phone', price: 1500 ,promotion: false},
-      { id: 3, name: 'camera', price: 1000 ,promotion: false},
+      { id: UUID.UUID(), name: 'pc', price: 3500, promotion: false },
+      { id: UUID.UUID(), name: 'phone', price: 1500, promotion: false },
+      { id: UUID.UUID(), name: 'camera', price: 1000, promotion: false },
     ];
+    for (let i = 0; i < 10; i++) {
+      this.products.push({
+        id: UUID.UUID(),
+        name: 'pc',
+        price: 3500,
+        promotion: false,
+      });
+      this.products.push({
+        id: UUID.UUID(),
+        name: 'camera',
+        price: 1000,
+        promotion: false,
+      });
+      this.products.push({
+        id: UUID.UUID(),
+        name: 'phone',
+        price: 1500,
+        promotion: false,
+      });
+    }
   }
 
-
-
-  public getAllProducts () : Observable <Product[]>{
-          return of(this.products);
-  }
-
-  public deleteProduct(index:number ) : Observable<Product[]>{
-    this.products = this.products.filter(p => p.id != index ) 
+  public getAllProducts(): Observable<Product[]> {
     return of(this.products);
   }
-}
+  public getPageProducts(page: number, size: number): Observable<PageProduct> {
+    let index = page * size;
+    let totalPages = ~~(this.products.length / size);
+    if (this.products.length % size) totalPages++;
+    let pageProduct = this.products.slice(index, index + size);
+    return of({
+      page: page,
+      size: size,
+      totalPages: totalPages,
+      products: pageProduct,
+    });
+  }
+  public deleteProduct(index: string): Observable<boolean> {
+    this.products = this.products.filter((p) => p.id != index);
+    console.log(this.products, 'ghid gan wwin service ');
+    return of(true);
+  }
+  public setPromotion(p: Product): Observable<Product> {
+    let index = this.products.indexOf(p);
+    this.products[index].promotion = !this.products[index].promotion;
+    return of(this.products[index]);
+  }
 
+  public searchProduct(
+    keyword: string,
+    page: number,
+    size: number
+  ): Observable<PageProduct> {
+    let result = this.products.filter((p) => p.name.includes(keyword));
+    let index = page * size;
+    let totalPages = ~~(result.length / size);
+    if (result.length % size) totalPages++;
+    let pageProduct = result.slice(index, index + size);
+    return of({
+      page: page,
+      size: size,
+      totalPages: totalPages,
+      products: pageProduct,
+    });
+  }
+}
